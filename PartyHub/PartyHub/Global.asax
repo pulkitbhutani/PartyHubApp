7bf041fbe3eb50653b574a53e81eb13e04b32409
Pulkit Bhutani <pulkitbhutani@outlook.com>
2018-10-19 20:17:31 +0530

    Initial App with Individual Authentication

---
+++ PartyHub/PartyHub/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PartyHub.MvcApplication" Language="C#" %>
---
+++ PartyHub/PartyHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PartyHub.Startup))]
+namespace PartyHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
